f311ea192570a3c4a5d0fc3fdb6045e95b1ba2b7
DH4NN <113597719+DeepBrain07@users.noreply.github.com>
2023-09-05 01:31:40 +0100

    Updating

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -31,3 +31,14 @@ class BasicAuth(Auth):
             return decoded_bytes.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+        """ Returns the user email and password from the Base64 decoded value
+        """
+        if decoded_base64_authorization_header is None or \
+                not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        decoded_base64_authorization_header = decoded_base64_authorization_header.split(':')
+        return decoded_base64_authorization_header[0], decoded_base64_authorization_header[1]
