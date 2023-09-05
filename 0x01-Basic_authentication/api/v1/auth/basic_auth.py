6ad2d3be76cae5c6eeebb4aac857e5e4ccdbb8a4
DH4NN <113597719+DeepBrain07@users.noreply.github.com>
2023-09-05 01:25:41 +0100

    Updating

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ This module inherits from the class 'Auth'
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -9,7 +10,7 @@ class BasicAuth(Auth):
     """
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
         """  returns the Base64 part of the
-            Authorization header for a Basic Authentication
+             Authorization header for a Basic Authentication
         """
         if authorization_header is None or \
                 not isinstance(authorization_header, str):
@@ -17,3 +18,16 @@ class BasicAuth(Auth):
         if authorization_header[:6] != 'Basic ':
             return None
         return authorization_header[6:]
+    
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """ Returns the decoded value of a
+            Base64 string base64_authorization_header
+        """
+        if base64_authorization_header is None or \
+                not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except Exception:
+            return None
