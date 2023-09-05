ab2a25b51b615c2eb9fe4f9ad5815356fa867133
DH4NN <113597719+DeepBrain07@users.noreply.github.com>
2023-09-05 15:59:12 +0100

    Updating

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -45,7 +45,6 @@ class BasicAuth(Auth):
         if ':' not in decoded_base64_authorization_header:
             return None, None
         decoded_base64_authorization_header = decoded_base64_authorization_header.split(':', 1)
-        print(decoded_base64_authorization_header[1])
         return decoded_base64_authorization_header[0], decoded_base64_authorization_header[1]
 
     def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
