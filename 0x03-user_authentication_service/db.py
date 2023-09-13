cfe7798aafb2fbdd3f49dc0fb53903a65503712a
DH4NN <113597719+DeepBrain07@users.noreply.github.com>
2023-09-13 23:13:54 +0100

    Task 2 completed

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
 from user import Base, User
 
 
@@ -15,7 +16,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -38,3 +39,15 @@ class DB:
             session.add(new_user)
             session.commit()
             return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ Find a user by the input argument
+        """
+        session = self._session
+        for key, val in kwargs.items():
+            filter_condition = {key: val}
+            user = session.query(User).filter_by(**filter_condition).first()
+            if not user:
+                raise NoResultFound()
+            return user
+        session.close()
